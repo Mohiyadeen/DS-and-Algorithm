588c278d9d14526383f49fee5dc310e38d149b94
Mohiyadeen <56976153+Mohiyadeen@users.noreply.github.com>
2020-03-04 11:36:44 +0530

    Update MergeSort.c

--- MergeSort.c
+++ MergeSort.c
@@ -23,23 +23,22 @@ void merge(int *a, int start, int mid, int end)
         a[k] = temp[k];
 }
 
-void merge_sort_recusion(int *a, int start, int end)
+void merge_sort_recursion(int *a, int start, int end)
 {
     int mid = (start+end)/2;
     if(start >= end)
         return;
     
-    merge_sort(a, start, mid);
-    merge_sort(a, mid+1, end);
+    merge_sort_recursion(a, start, mid);
+    merge_sort_recursion(a, mid+1, end);
     merge(a, start, mid, end);
 }
 
 int main()
 {
-    int i =0, mid=0;
+    int i =0;
     int a[] = {12, 23, 45, 55, 66, 22, 10};
     int no = (sizeof(a)/sizeof(a[0]));
-    mid = (0+no)/2;
     merge_sort_recursion(a, 0, no-1);
     for(i =0 ; i <= no -1;i++)
     printf("\n%d",a[i]);
